46ff0d218739a2dbeba6654f34101d6b31606377
Adnan <adnan@websemantics.ca>
2012-12-29 21:51:29 -0500

    Update javascripts/main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -83,14 +83,14 @@ require(["dojo/ready", "dojo/on", "dojo/dom", "dojo/_base/lang", "dojo/dom-style
                 var left = new ContentPane({
                     region: 'leading',
                     splitter: true,
-                    id: id+"_left_panel",
+                    id: id+"_left_pane",
                     content: lang.replace(buttons_content, {id: id}) + lang.replace(media_content, {id: id})
                 });
 
                 var right = new ContentPane({
                     region: 'center',
                     splitter: true,
-                    id:  id+"_right_panel",
+                    id:  id+"_right_pane",
                 });
 
                 right.addChild(timeline);
